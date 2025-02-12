d30e4c3f37dfceea1cc9eb3bdbde4176dd995333
Anzai <55052716+owari-hash@users.noreply.github.com>
2025-02-12 17:14:29 +0800

    test

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
+        Circle circle = new Circle(2);
+        Square square = new Square(2);
 
         Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(square);
+        Renderer renderer2 = new Renderer(circle);
 
         renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -1,13 +1,14 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Square implements Shape {
-    public double sideLen;
+    private double sideLen;
 
     public Square(double sideLen) {
         this.sideLen = sideLen;
     }
-    
+
     public double getArea() {
         return sideLen * sideLen;
     }
+
 }
